ac30b78e3db99760c473279016862962cd322d43
gleiSUN <go5624314@gmail.com>
2025-05-27 19:25:58 -0300

    movimentação player

---
+++ taxa_action-main/atores/player/scripts/player_script.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+var input: Vector2 = Vector2.ZERO
+
+var velocidade: float = 100.0
+
+func get_input():
+	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	input = input.normalized()  
+	
+func character_movement():
+	velocity = input * velocidade
+	
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	get_input()
+	character_movement()
+	move_and_slide()
+	
