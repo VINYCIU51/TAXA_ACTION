ec511e361b5be2f0e7f33e656a6d03c4451c2fcc
gleiSUN <go5624314@gmail.com>
2025-06-05 16:51:11 -0300

    Correção de fisica nos tilesets e transição de fases

---
+++ scripts/gerenciador_de_cenas.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var cenas : Dictionary = {"Level 1":"res://worlds/cena_inicial/quarto.tscn",
+						  "Level 2": "res://worlds/tutorial.tscn"}
+
+func transition_to_scene(level : String):
+	var scene_path : String = cenas.get(level)
+	
+	if scene_path != null:
+		await get_tree().create_timer(1.0).timeout
+		get_tree().change_scene_to_file(scene_path)
