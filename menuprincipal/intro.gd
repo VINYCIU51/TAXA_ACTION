6d9b7007be3ab59c80f1a7c5898c16ae47a8f31e
gleiSUN <go5624314@gmail.com>
2025-06-13 00:56:43 -0300

    loading screen finalizada

--- menuprincipal/intro.gd
+++ menuprincipal/intro.gd
@@ -1,5 +1,5 @@
 extends Control
 
-func _on_intro_animation_finished(anim_name: StringName):
-	get_tree().change_scene_to_file("res://menuprincipal/menuprincipal.tscn")
-	#GerenciadorDeCenas.transition_to_scene("Menu_principal")
+func _on_intro_animation_finished(_anim_name: StringName):
+	#get_tree().change_scene_to_file("res://menuprincipal/menuprincipal.tscn")
+	GerenciadorDeCenas.transition_to_scene("Menu_principal")
